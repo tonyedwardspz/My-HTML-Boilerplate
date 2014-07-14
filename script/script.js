e5e04cf027ca980a73ad58ee25ffe610dca9f05e
Tony Edwards <tony.edwards@gmail.com>
2014-07-15 00:19:34 +0100

    0.0.3
    - finished responsive menu with jQuery
    - changed the main breakpoint to 800px

--- script/script.js
+++ script/script.js
@@ -1,7 +1,29 @@
 
+headerMargins();
 
 
-// header menu toggle
-$(".burgerMenu").click(function(e){
-    $(".navBar").toggle();
+$(document).ready(function(){
+    $(".burgerMenu").click(function(e){
+        $(".navBar").toggle();
+    });
+    
 });
+
+
+$(window).resize(function(){
+    offset = 0;
+    headerMargins();
+    
+});
+
+// Burger menu right margin
+// get the offset of the 'main' element and apply to the burger menu
+function headerMargins(){
+    offset = $("main").offset();
+    
+    var burgerMenu = $(".burgerMenu");
+    burgerMenu.css("right", offset.left);
+    
+    var siteNameElm = $(".site-name");
+    siteNameElm.css("left", offset.left);
+}
